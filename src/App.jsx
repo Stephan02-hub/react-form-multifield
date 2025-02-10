a427debe7f58347fbef03a8bda3c27543a4dd052
Stephan02-hub <braghesesteven@gmail.com>
2025-02-10 13:57:21 +0100

    addfeat

--- src/App.jsx
+++ src/App.jsx
@@ -2,60 +2,100 @@ import { useState } from 'react'
 
 
 export default function App() {
-  const [firstProduct, setFirstProduct] = useState ("");
-  const [ItalianList, setItalianList] = useState(["pasta" , "mandolino", "pizza"])
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    
-    setItalianList(currentState => [...currentState, firstProduct] )
-
-    setFirstProduct("")
-  }
-
-  const emptyList = () => {
-    setItalianList([])
-  }
-
-  const handDelete = (productDelete) => {
-    setItalianList((currentState) => 
-      currentState.filter((product) =>
-      product !== productDelete)
-    );
+  const [list, setList] = useState([]);
+  const [article, setArticle] = useState({
+    title: "",
+    autore: "",
+    contenuto: "",
+    select: "",
+    available: false,
+  });
+  const handleArticle = (fieldName, value) => {
+    setArticle((currentFormData) => ({
+      ...currentFormData,
+      [fieldName]: value,
+    }));
   };
-
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setList((currentList) => [...currentList, article]);
+    setArticle({
+      title: "",
+      autore: "",
+      contenuto: "",
+      select: "",
+      available: false,
+    });
+  };
   return (
-
-    <div>
-
-      <h1>Italian List</h1>
-      <ul>
-        {
-          ItalianList.map((product, index) => (
-            <li key={index}>
-              {product} <button onClick={() => handDelete(product)}>❌</button>
-              
-            </li>
-          ))}
-      </ul>
-
-      <button onClick={emptyList}>Cancella lista</button>
-      <hr />
-      <form onSubmit={handleSubmit}>
-
-        <input
-          type="text"
-          value={firstProduct}
-          onChange={(e) => {
-            setFirstProduct(e.target.value);
-          }}
-          placeholder="Scrivi il prodotto"
-        />
-
-        <button type="submit">invia</button>
-      </form>
-    </div>
-  )
+    <main>
+      <div className="container">
+        <h1>LISTA</h1>
+        <div className="list">
+        <ul>
+            {list.map((item) => (
+              <li key={item.title}>
+                {item.title} - {item.autore} - {item.contenuto} - {item.select}{" "}
+                - {item.available ? "disponibile" : "non disponibile"}
+              </li>
+            ))}
+          </ul>
+          <hr />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="area-testo"
+              type="text"
+              placeholder="Inserisci il titolo"
+              value={article.title}
+              onChange={(event) => handleArticle("title", event.target.value)}
+            />
+            <br />
+            <input
+              className="area-testo"
+              type="text"
+              placeholder="Inserisci l'autore"
+              value={article.autore}
+              onChange={(event) => handleArticle("autore", event.target.value)}
+            />
+            <br />
+            <input
+              className="area-testo"
+              type="text"
+              placeholder="Inserisci il contenuto"
+              value={article.contenuto}
+              onChange={(event) =>
+                handleArticle("contenuto", event.target.value)
+              }
+            />
+            <br />
+            <label for="scelta">Scegli un'opzione:</label>
+            <select
+              className="select-area"
+              value={article.select}
+              onChange={(event) => handleArticle("select", event.target.value)}
+            >
+              <option value="FrontEnd">FrontEnd</option>
+              <option value="BackEnd">BackEnd</option>
+              <option value="UI">UI</option>
+              <option value="UX">UX</option>
+            </select>
+            <br />
+            <label className="checkbox-area">
+              <p>Clicca la spunta se disponibile</p>
+              <input
+                type="checkbox"
+                value={article.available}
+                onChange={(event) =>
+                  handleArticle("available", event.target.checked)
+                }
+              />
+            </label>
+            <button className="btn">AGGIUNGI</button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
 }
+
+  
